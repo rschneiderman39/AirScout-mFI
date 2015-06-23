6ede9a3fecc09fe9a92908959294ff176dfba363
unknown <cwelch@AH860054.txt.textron.com>
2015-06-23 12:10:25 -0700

    WifiAdmin plugin working for Android.

--- wifiScout/www/app/services/infoService.js
+++ wifiScout/www/app/services/infoService.js
@@ -1,18 +1,27 @@
+/*
+* infoService provides an interface for getting AP information
+* from the device.
+*
+* getInfo - Returns a deferred object which will resolve to the data fetched
+            and stored by updateInfo.  updateInfo should always be called first.
+* updateInfo - Fetch and store AP info from the device.
+*
+*/
 app.factory('infoService', function() {
   var _info = $.Deferred().reject();
   var service = {};
 
   service.getInfo = function() {
-      return _info;
-  };
+    return _info;
+  }
 
   service.updateInfo = function() {
     _info = $.Deferred();
     // plugin api call
     window.plugins.WifiAdmin.getWifiInfo(
       // on success
-      function(data) {
-        _info.resolve(data);
+      function(info) {
+        _info.resolve(info);
       },
       // on failure
       function() {
