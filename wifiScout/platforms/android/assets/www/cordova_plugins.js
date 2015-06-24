fa10e780d82c46ceda93648ae02f575b3ff6c6a8
rschneiderman39 <rschneiderman379@gmail.com>
2015-06-24 16:05:30 -0700

    added another icon

--- wifiScout/platforms/android/assets/www/cordova_plugins.js
+++ wifiScout/platforms/android/assets/www/cordova_plugins.js
@@ -1,33 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/network.js",
-        "id": "org.apache.cordova.network-information.network",
-        "clobbers": [
-            "navigator.connection",
-            "navigator.network.connection"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.network-information/www/Connection.js",
-        "id": "org.apache.cordova.network-information.Connection",
-        "clobbers": [
-            "Connection"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-wifi/www/WifiAdmin.js",
-        "id": "cordova-plugin-wifi.WifiAdmin",
-        "clobbers": [
-            "window.plugins.WifiAdmin"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.network-information": "0.2.15",
-    "cordova-plugin-wifi": "0.5.0"
-}
+{}
 // BOTTOM OF METADATA
 });
