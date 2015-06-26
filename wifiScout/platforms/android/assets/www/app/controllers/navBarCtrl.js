545a5dccce20a509fb9e6da759a40db4f63431d7
rschneiderman39 <rschneiderman379@gmail.com>
2015-06-26 14:59:48 -0700

    breaking things

--- wifiScout/platforms/android/assets/www/app/controllers/navBarCtrl.js
+++ wifiScout/platforms/android/assets/www/app/controllers/navBarCtrl.js
@@ -1,12 +1,20 @@
-app.controller('navBarCtrl', ['$scope', '$timeout', '$location' 'cordovaService',
+app.controller('navBarCtrl', ['$scope', '$timeout', '$location', 'cordovaService',
   function($scope, $timeout, $location, cordovaService) {
   	cordovaService.ready.then(
-  		var pageLeft = function(view) {
+  		function resolved() {
+  		console.log("IN table controller");
+  		$scope.pageLeft = function(view) {
+  			console.log("HEY SWIPING LEFT");
         $location.path(view);
       }
 
       var pageRight = function() {
           
       }
+    },
+    function rejected() {
+        console.log("navBarCtrl is unavailable because Cordova is not loaded.")
+      }
   	); 
+
   }]);
