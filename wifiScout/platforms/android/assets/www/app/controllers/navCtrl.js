4aeff0a45da0a32efa0b5c9fcb7e47f4ff53311c
unknown <cwelch@AH860054.txt.textron.com>
2015-06-26 14:28:06 -0700

    changed the selected icons to a more solid green

--- wifiScout/platforms/android/assets/www/app/controllers/navCtrl.js
+++ wifiScout/platforms/android/assets/www/app/controllers/navCtrl.js
@@ -2,7 +2,19 @@ app.controller('navCtrl', ['$scope', 'cordovaService',
   function($scope, cordovaService) {
     cordovaService.ready.then(
       function resolved() {
-        $scope.selected = 'Settings';
+        $scope.select = function(id) {
+          var old_selected = $('#' + _selected)[0],
+              new_selected = $('#' + id)[0];
+
+          console.log(old_selected.src);
+
+          old_selected.src = old_selected.src.replace("-selected", "");
+          new_selected.src = new_selected.src.replace(".png", "-selected.png");
+
+          _selected = id;
+        }
+
+        var _selected = 'settings-img';
       },
       function rejected() {
         console.log("navCtrl is unavailable because Cordova is not loaded.");
