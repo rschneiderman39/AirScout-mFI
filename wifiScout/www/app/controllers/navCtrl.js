8b4bbb583ea4f753a913d04106cb5e48907f7273
unknown <cwelch@AH860054.txt.textron.com>
2015-06-26 16:20:31 -0700

    Improved UI responsiveness for table

--- wifiScout/www/app/controllers/navCtrl.js
+++ wifiScout/www/app/controllers/navCtrl.js
@@ -7,8 +7,6 @@ app.controller('navCtrl', ['$scope', '$location', 'cordovaService',
           var old_selected = $('#' + _selected)[0],
               new_selected = $('#' + id)[0];
 
-          console.log(old_selected.src);
-
           old_selected.src = old_selected.src.replace("-selected", "");
           new_selected.src = new_selected.src.replace(".png", "-selected.png");
 
