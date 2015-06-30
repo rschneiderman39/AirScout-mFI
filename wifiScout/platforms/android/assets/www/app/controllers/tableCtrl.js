ef43ec26bca2025480948c4a7de08dc72bf8394c
unknown <cwelch@AH860054.txt.textron.com>
2015-06-30 13:58:28 -0700

    merge conflicts

--- wifiScout/platforms/android/assets/www/app/controllers/tableCtrl.js
+++ wifiScout/platforms/android/assets/www/app/controllers/tableCtrl.js
@@ -1,113 +1,81 @@
-app.controller('tableCtrl', ['$scope', '$timeout', '$location', 'APService',
-                             'filterService', 'cordovaService', '$element',
-  function($scope, $timeout, $location, APService, filterService, cordovaService, $element) {
+app.controller('tableCtrl', ['$scope', '$timeout', 'APService', 'filterService',
+                             'filterSettingsService', 'cordovaService',
+  function($scope, $timeout, APService, filterService, filterSettingsService, cordovaService) {
     cordovaService.ready.then(
       function resolved() {
-        $scope.modal = {
-          allAPs: [],
-          selectedAPs: [],
-          selector: 'SSID',
-          buttonText: 'List by MAC',
-          toggleSelector: function() {
-            if (this.selector === 'SSID') {
-              this.buttonText = 'List by SSID';
-              this.selector = 'BSSID';
-            } else {
-              this.buttonText = 'List by MAC';
-              this.selector = 'SSID';
-            }
-          },
-          showAll: function() {
-            this.selectedAPs = this.allAPs.slice();
-            _showAll = true;
-            _forceUpdate();
-          },
-          hideAll: function() {
-            this.selectedAPs = [];
-            _showAll = false;
-            _modalToTable();
-          }
-        };
-
+        // Settings for this session
         $scope.table = {
-          selectedAPs: [],
-          predicate: 'SSID',
-          reverse: false,
+          selectedAPs: [],         // The AP objects representing the APs we want to display
+          sortPredicate: 'SSID',   // Sorting options
+          sortReverse: false,      // ..
+          // Change the sort predicate, or reverse the sort direction
           order: function(predicate) {
-            this.reverse = (this.predicate === predicate) ? !this.reverse : false;
-            this.predicate = predicate;
+            this.sortReverse = (this.sortPredicate === predicate) ? !this.sortReverse : false;
+            this.sortPredicate = predicate;
           }
         };
 
-        $scope.isSelected = function(MAC) {
-          return $scope.selected == MAC;
-        };
-
-        $scope.setSelected = function(level, MAC) {
-          console.log("**** AP HAS BEEN CLICKED *****");
-          console.log("Signal strength for this AP is: " + level);
-          $scope.selected = MAC;
-        };
-
+        // true: show all APs, including new ones as they are discovered
+        // false: only show selected APs
         var _showAll = true;
+
+        // Because AP data is constantly changing, we use MAC addresses
+        // as a permanent ID for the APs we've selected
         var _selectedBSSIDs = [];
 
-        // Updates the table every quantum
-        var _update = function() {
+        // Update the table whenever settings are changed
+        var _onSettingsChange = function(settings) {
+          _selectedBSSIDs = settings.selectedBSSIDs.slice();
+          _showAll = settings.showAll;
           _forceUpdate();
-          $timeout(_update, 1000);
+          filterSettingsService.getSettings('table').done(_onSettingsChange);
         };
 
-        // Updates the table immediately
+        // Save our sort settings to the settings service
+        var _pushSortSettings = function() {
+          filterSettingsService.setSortPredicate('table', $scope.table.sortPredicate);
+          filterSettingsService.setSortReverse('table', $scope.table.sortReverse);
+        };
+
+        // Update the table now
         var _forceUpdate = function() {
           if (_showAll) {
-            $scope.table.selectedAPs = APService.namedAPs;
+            $scope.table.selectedAPs = APService.getNamedAPs();
           } else {
+            // Show only the APs whose BSSIDs match those we've selected
             $scope.table.selectedAPs = filterService.filter(
-              APService.namedAPs,
+              APService.getNamedAPs(),
               _selectedBSSIDs
             );
           }
-        }
+        };
 
-        // Updates the state of the modal each time it's opened
-        var _initModal = function() {
-          $scope.modal.allAPs = APService.namedAPs;
-          if (_showAll) {
-            $scope.modal.selectedAPs = $scope.modal.allAPs.slice();
-          } else {
-            $scope.modal.selectedAPs = filterService.filter(
-              $scope.modal.allAPs,
-              _selectedBSSIDs
-            );
-          }
-        }
-
-        // Updates the table with the user's new selection
-        var _modalToTable = function() {
-          _selectedBSSIDs = $scope.modal.selectedAPs.map(
-            function(ap) {return ap.BSSID; }
-          );
+        // Update the table every quantum
+        var _update = function() {
           _forceUpdate();
+          $timeout(_update, 500);
         }
 
-        // When a checkbox is clicked, turn off showAll mode, and
-        // update the table as soon as angular has dealt with the two way
-        // data rebinding.
-        var _onCheckboxClick = function() {
-          _showAll = false;
-          $timeout(_modalToTable, 100);
-        }
+        // Pull settings from filterSettingsService, and start waiting on settings changes
+        filterSettingsService.getSettingsImmediate('table').done(
+          function(settings) {
+            $scope.table.sortPredicate = settings.sortPredicate;
+            $scope.table.sortReverse = settings.sortReverse;
+            _selectedBSSIDs = settings.selectedBSSIDs.slice();
+            _showAll = settings.showAll;
+            filterSettingsService.getSettings('table').done(_onSettingsChange);
+          }
+        );
 
-        // Init
-        $('#tableModal').on('show.bs.modal', _initModal);
-        $('#tableModal').on('hide.bs.modal', _modalToTable);
-        $('#tableModalList').on('click', _onCheckboxClick);
-
+        // Start table update loop
         _update();
+
+        // When we navigate away, remember our sort settings
+        $scope.$on('$destroy', _pushSortSettings);
+
       },
       function rejected() {
-        console.log("tableCtrl is unavailable because Cordova is not loaded.")
+        console.log("tableCtrl is unavailable because Cordova is not loaded.");
       }
     );
-}]);
+  }]);
