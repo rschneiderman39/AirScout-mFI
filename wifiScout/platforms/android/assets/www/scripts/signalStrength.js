c32606a2e72bf057d70f6bda8a56d14695a9214a
rschneiderman39 <rschneiderman379@gmail.com>
2015-07-13 15:27:02 -0700

    fixed signal strength view, now is scalable and speedometer larger

--- wifiScout/platforms/android/assets/www/scripts/signalStrength.js
+++ wifiScout/platforms/android/assets/www/scripts/signalStrength.js
@@ -1,7 +1,7 @@
-var speedometerW = .75 * document.deviceHeight;
-var speedometerH = .5 * speedometerW;
+var speedometerW = .95 * document.deviceHeight;
+var speedometerH = .60 * speedometerW;
 
-var viewHeight = document.deviceHeight - 225;
+var viewHeight = document.deviceHeight - 150;
 
 $('.scrollable-list	').css('height', viewHeight);
 $('.speedometer').append("<center><canvas id='foo' height='" + speedometerH + "' width='" + speedometerW + "'></canvas></center>");
