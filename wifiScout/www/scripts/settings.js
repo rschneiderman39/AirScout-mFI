67cdcd970240171d31769dabef972226e9f5fa98
rschneiderman39 <rschneiderman379@gmail.com>
2015-07-16 09:41:10 -0700

    polished settings view, looks good now

--- wifiScout/www/scripts/settings.js
+++ wifiScout/www/scripts/settings.js
@@ -1,1 +1,7 @@
 $('#touchLayer').css('height', document.deviceHeight - document.topBarHeight);
+
+/*var panelWidth = (9/10) * document.deviceWidth;
+var panelPadding = (1/20) * document.deviceWidth;
+
+$('.panel-group').css('width', panelWidth);
+$('.panel-group').css('right', -(panelPadding));*/
