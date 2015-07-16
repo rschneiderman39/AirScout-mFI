5f1b91a0c9f606b484382442463ac3b7eeab3746
rschneiderman39 <rschneiderman379@gmail.com>
2015-07-16 09:20:37 -0700

    fixed issue with settings menu bar and nav bar animation

--- wifiScout/www/app/controllers/navCtrl.js
+++ wifiScout/www/app/controllers/navCtrl.js
@@ -63,6 +63,154 @@ app.controller('navCtrl', ['$scope', '$state', '$timeout', 'cordovaService',
           $scope.setActive(view);
         };
 
+        $scope.startIntro = function () {
+          var intro = introJs();
+          switch (activeView) {
+            case "settings":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Settings!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is a tooltip."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "This is the plain view"
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "This is the fun and interesting view!"
+                  }
+                ]
+              });
+              break;
+            case "channelGraph":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Channel graph!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is a tooltip."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "This is the plain view"
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "This is the fun and interesting view!"
+                  }
+                ]
+              });
+              break;
+            case "signalStrength":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Signal strength!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is a tooltip."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "This is the plain view"
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "This is the fun and interesting view!"
+                  }
+                ]
+              });
+              break;
+            case "timeGraph":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Time graph!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is a tooltip."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "This is the plain view"
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "This is the fun and interesting view!"
+                  }
+                ]
+              });
+              break;
+            case "APTable":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Welcome to the AP Table view!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is the table where you can view information "
+                            + "about each in-range access point."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "By tapping on a table column label, you can "
+                            + "sort the information by the specific label."
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "Level represents the signal strength!"
+                  },
+                  {
+                    element: document.querySelector('#step4'),
+                    intro: "The filter button allows you to search for "
+                            + "and select only certain access points to be "
+                            + "displayed on the table.",
+                    position: "right"
+                  }
+                ]
+              });
+              break;
+            case "channelTable":
+              intro.setOptions({
+                showStepNumbers: false,
+                steps: [
+                  {
+                    intro: "Channel table!"
+                  },
+                  {
+                    element: document.querySelector('#step1'),
+                    intro: "This is a tooltip."
+                  },
+                  {
+                    element: document.querySelector('#step2'),
+                    intro: "This is the plain view"
+                  },
+                  {
+                    element: document.querySelector('#step3'),
+                    intro: "This is the fun and interesting view!"
+                  }
+                ]
+              });
+              break;
+          }
+          intro.start();
+        };
+
         var activeView = "settings",
             navTimeout = null,
             NAV_SHOW_INTERVAL = 2000;
@@ -73,4 +221,4 @@ app.controller('navCtrl', ['$scope', '$state', '$timeout', 'cordovaService',
         console.log("navCtrl is unavailable because Cordova is not loaded.");
       }
     )
-  }])
+}])
