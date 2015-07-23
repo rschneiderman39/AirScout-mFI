9971f05f56c0a3574762f08d975b42fcf2d32aad
rschneiderman39 <rschneiderman379@gmail.com>
2015-07-23 15:37:16 -0700

    time graph view design finalized

--- wifiScout/www/scripts/signalStrength.js
+++ wifiScout/www/scripts/signalStrength.js
@@ -1,5 +1,3 @@
 //	Dynamically scale the size of the speedometer depending on device size
 var gaugeHeight = document.deviceHeight * 0.70;
-//var gaugeWidth = gaugeHeight * 2.50;
 $('#chartdiv').css('height', gaugeHeight);
-//$('#chartdiv').css('width', gaugeWidth);
