c5b9323516103a662806c4b2a408189e9ce307b7
rschneiderman39 <rschneiderman379@gmail.com>
2015-07-23 16:05:13 -0700

    finalized ap table view design

--- wifiScout/www/scripts/APTable.js
+++ wifiScout/www/scripts/APTable.js
@@ -1,13 +1,9 @@
 // Set table height and nav bar interaction layer to be height of
 //	the viewport minus the height of the top bar
+$('.table-striped thead').css('height', '40px');
 var tHeadHeight = $('.table-striped thead').height();
 
 $('.table-striped thead').css('top', document.topBarHeight);
 $('.table-striped tbody').css('top', document.topBarHeight + tHeadHeight);
 
-$('.table-striped tbody').css('height', (document.deviceHeight - document.topBarHeight - tHeadHeight) );
-
-$('.table-striped thead').css('width', document.deviceWidth);
-$('.table-striped tbody').css('width', document.deviceWidth);
-
-$('#touchLayer').css('height', document.deviceHeight - document.topBarHeight);
+$('.table-striped tbody').css('height', (document.deviceHeight - document.topBarHeight - tHeadHeight - 10) );
