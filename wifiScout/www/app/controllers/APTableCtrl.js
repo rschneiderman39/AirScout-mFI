a2baf5395bec60917631361954edb1aa509dad10
chris7394 <chris7394@gmail.com>
2015-07-30 20:11:51 -0700

    Comments

--- wifiScout/www/app/controllers/APTableCtrl.js
+++ wifiScout/www/app/controllers/APTableCtrl.js
@@ -1,3 +1,4 @@
+/** Controller for the AP Table view.  Handles data fetching, table updating, and sorting. */
 app.controller('APTableCtrl', ['$scope', 'accessPoints',
 'globalSettings', 'APTableState', 'setupService', function($scope,
 accessPoints, globalSettings, APTableState, setupService) {
@@ -7,11 +8,21 @@ accessPoints, globalSettings, APTableState, setupService) {
   };
 
   setupService.ready.then(function() {
+    /** Make strings available in view */
     $scope.strings = globals.strings;
-    $scope.selectedAPData = [];         // The AP objects representing the APs we want to display
-    $scope.sortPredicate = undefined;    // Sorting options
-    $scope.sortReverse = undefined;       // ..
-    // Change the sort predicate, or reverse the sort direction
+    /** AP data to be displayed in the table. @type {Array.<APData>} */
+    $scope.selectedAPData = [];  
+    /** Predicate to be used by angular sort. @type {string|function} */
+    $scope.sortPredicate = undefined;   
+    /** Reverse option to be used by angular sort. @type {string|function} */
+    $scope.sortReverse = undefined;     
+    
+    /** Triggered whenever a sort arrow is clicked. The sort predicate is changed to the new predicate.
+       If the new predicate is the same as the current one, the sort direction is reversed. If 'SSID'
+       is selected as the predicate, a custom ordering function is substituted instead.
+       
+       @param {string|function} predicate - The new argument for angular orderBy.
+    */
     $scope.order = function(predicate) {
       if (predicate === 'SSID') {
         $scope.sortReverse = ($scope.sortPredicate === $scope.sortSSID) ? !$scope.sortReverse : false;
@@ -22,38 +33,46 @@ accessPoints, globalSettings, APTableState, setupService) {
       }
     };
 
-    $scope.sortSSID = globals.utils.customSSIDSort;
+    /** Used in place of a string predicate to sort access points by SSID. @type {function} */ 
+    $scope.sortSSID = globals.utils.customSSIDSort; 
 
-    var showAll = true,
-        selectedBSSIDs = [];
+    var showAll = true;      /* True: display all access points regardless of selection.
+                                False: display only selected access points. */
+                             
+    var selectedBSSIDs = []; /* Array of BSSID strings representing the user's current access
+                                point selection */
 
 
-    // Update the table whenever filter settings are changed
-    var updateSelection = function(settings) {
-      selectedBSSIDs = settings.selectedBSSIDs;
-      showAll = settings.showAll;
+    /** Update the locally stored selection whenever the user changes the selection with the
+       filter menu.
+       
+       @param {Selection} newSelection - The new selection.
+    */
+    var updateSelection = function(newSelection) {
+      selectedBSSIDs = newSelection.selectedBSSIDs;
+      showAll = newSelection.showAll;
       update();
       globalSettings.awaitNewSelection('APTable').done(updateSelection);
     };
 
-    // Save our sort settings to the settings service
+    /** Remember our sort settings for next time. */
     var storeSortSettings = function() {
       APTableState.sortPredicate($scope.sortPredicate);
       APTableState.sortReverse($scope.sortReverse);
     };
 
-    // Update the table now
+    /** Pull in new data and update the table. */
     var update = function() {
       $scope.$apply(function() {
         if (showAll) {
           $scope.selectedAPData = accessPoints.getAll();
         } else {
-          // Show only the APs whose BSSIDs match those we've selected
           $scope.selectedAPData = accessPoints.getSelected(selectedBSSIDs);
         }
       });
     };
 
+    /* Manually scale the view to the device where needed. */
     var prepView = function() {
       $('.table-striped thead').css('height', '40px');
       var tableHeadHeight = $('.table-striped thead').height();
@@ -64,13 +83,16 @@ accessPoints, globalSettings, APTableState, setupService) {
       $('.table-striped tbody').css('height', (globals.format.window.height - globals.format.topBar.height - tableHeadHeight - 10) );
     };
 
+    /** Perform controller setup. */
     var init = function() {
       prepView();
 
+      /** Load the current access point selection for this view */
       var selection = globalSettings.getSelection('APTable');
       selectedBSSIDs = selection.selectedBSSIDs;
       showAll = selection.showAll;
 
+      /** Load the sort settings from last time view was used. */
       var predicate = APTableState.sortPredicate();
       if (predicate === 'SSID') {
         $scope.sortPredicate = $scope.sortSSID;
@@ -83,6 +105,7 @@ accessPoints, globalSettings, APTableState, setupService) {
 
       var updateLoop = setInterval(update, prefs.updateInterval);
 
+      /** Runs on view unload */
       $scope.$on('$destroy', function() {
         clearInterval(updateLoop);
         storeSortSettings();
